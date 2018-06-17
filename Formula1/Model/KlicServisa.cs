141e890ab366eccb40a3f789ea7a9f2f834faba4
Danijel Volk <inf.1267@scng.si>
2018-06-17 19:04:09 +0200

    Add project files.

---
+++ Formula1/Model/KlicServisa.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Model
+{
+  public class KlicServisa
+    {
+        public static async Task PopulateDrivers(ObservableCollection<Driver> ab)
+        {
+            
+            //namestiomo z NuGet package - Microsoft.AspNet.WebApi.Client
+            string url = "https://ergast.com/api/f1/2004/1/results.json";
+            //zbirka
+            
+            Mrdata vsa1;
+            using (var klient = new HttpClient())
+            {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                Rootobject ro1;
+                ro1 = await sp.Content.ReadAsAsync<Rootobject>();
+                vsa1 = ro1.MRData;
+            }
+            foreach (var x in vsa1.RaceTable.Races)
+            {
+                foreach (var y in x.Results)
+                {
+                Driver a = new Driver();
+                    a.givenName = y.Driver.givenName;
+                    a.familyName = y.Driver.familyName;
+                    a.dateOfBirth = y.Driver.dateOfBirth;
+                    a.nationality = y.Driver.nationality;
+                    a.url = y.Driver.url;
+                    ab.Add(a);
+                }
+            }
+        }
+        public static async Task PopulateKonstruktor(ObservableCollection<Constructor> ab)
+        {
+
+            //namestiomo z NuGet package - Microsoft.AspNet.WebApi.Client
+            string url = "https://ergast.com/api/f1/2004/1/results.json";
+            //zbirka
+
+            Mrdata vsa1;
+            using (var klient = new HttpClient())
+            {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                Rootobject ro1;
+                ro1 = await sp.Content.ReadAsAsync<Rootobject>();
+                vsa1 = ro1.MRData;
+            }
+            foreach (var x in vsa1.RaceTable.Races)
+            {
+                foreach (var y in x.Results)
+                {
+                    Constructor a = new Constructor();
+                    a.name = y.Constructor.name;
+                    a.nationality = y.Constructor.nationality;
+                    ab.Add(a);
+                }
+            }
+        }
+        public static async Task PopulateRezultati(ObservableCollection<Result> ab, ObservableCollection<Race> ra, ObservableCollection<Driver> dri, ObservableCollection<Constructor> con)
+        {
+
+            //namestiomo z NuGet package - Microsoft.AspNet.WebApi.Client
+            string url = "https://ergast.com/api/f1/2004/1/results.json";
+            //zbirka
+
+            Mrdata vsa1;
+            using (var klient = new HttpClient())
+            {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                Rootobject ro1;
+                ro1 = await sp.Content.ReadAsAsync<Rootobject>();
+                vsa1 = ro1.MRData;
+            }
+            foreach (var x in vsa1.RaceTable.Races)
+            {
+                Race a = new Race();
+                a.season = x.season;
+                a.raceName = x.raceName;
+                ra.Add(a);
+                foreach (var y in x.Results)
+                {
+                    Result b = new Result();
+                    b.position = y.position;
+                    b.points = y.points;
+                    ab.Add(b);
+                    Driver d = new Driver();
+                    d.familyName = y.Driver.familyName;
+                    d.givenName = y.Driver.givenName;
+                    dri.Add(d);
+                    Constructor c = new Constructor();
+                    c.name = y.Constructor.name;
+                    con.Add(c);
+                }
+            }
+        }
+    }
+}
---
+++ Formula1/PregledPosamezneDirke.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Formula1.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Formula1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PregledPosamezneDirke : Page
+    {
+
+
+
+        public ObservableCollection<Result> VsiRezultiDirke { get; set; }
+        public ObservableCollection<Race> VsiRaceDirke { get; set; }
+        public ObservableCollection<Constructor> VsiKonstruktorjiDirke { get; set; }
+        public ObservableCollection<Driver> VsiVoznikiDirke { get; set; }
+        public PregledPosamezneDirke()
+        {
+            this.InitializeComponent();
+            VsiRezultiDirke = new ObservableCollection<Result>();
+            VsiRaceDirke = new ObservableCollection<Race>();
+            VsiKonstruktorjiDirke = new ObservableCollection<Constructor>();
+            VsiVoznikiDirke = new ObservableCollection<Driver>();
+        }
+        public async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            masterList4.ItemsSource = null;
+            masterList3.ItemsSource = null;
+            masterList2.ItemsSource = null;
+            await KlicServisa.PopulateRezultati(VsiRezultiDirke, VsiRaceDirke, VsiVoznikiDirke, VsiKonstruktorjiDirke);
+        }
+
+        private void masterList1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var izbranaSezona = (Race)e.ClickedItem;
+            masterList2.ItemsSource = null;
+           masterList2.ItemsSource = VsiRezultiDirke;
+            masterList3.ItemsSource = null;
+            masterList3.ItemsSource = VsiVoznikiDirke;
+            masterList4.ItemsSource = null;
+            masterList4.ItemsSource = VsiKonstruktorjiDirke;
+        }
+
+    }
+}
